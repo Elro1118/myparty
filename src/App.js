c8b2da97d9c4376abfdb2c14c8b64e7354be9977
Elro1118 <silvana.rojas8119@gmail.com>
2019-02-24 01:09:25 -0500

    improvingPhaseIV

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
 import Movie from './components/Movie'
-import data from './data/movies.json'
+// import data from './data/movies.json'
 
 class App extends Component {
   state = {
-    moviesdata: data.results
+    moviesdata: [] // data.results
+  }
+
+  componentDidMount() {
+    fetch(
+      'https://api.themoviedb.org/3/discover/movie?primary_release_year=1989&sort_by=popularity.desc&api_key=fae89ce6616cd4e865bdfb392495d453'
+    )
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          moviesdata: data.results
+        })
+      })
   }
 
   render() {
-    // const sorted_date
-
     return (
       <>
         {this.state.moviesdata
